28d6ac425f3b01ecc809e1f8fb748af951210264
Dillon Waugh <waughdillon66@gmail.com>
2021-10-24 08:16:29 -0400

    JS complete

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+window.onload=function(){
+    var form = document.querySelector('form');
+    var message= document.getElementsByClassName('message')[0];
+    var regexp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var email = document.getElementById('email');
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        var mail=email.value;
+    if(mail==="" || !(mail.match(regexp))){
+        message.textContent= `Please enter a valid email address.`;
+    }
+    else{
+        message.textContent= `Thank you! Your email address ${mail} has been added to our mailing list!`;
+    }
+       
+    })
+
+}
